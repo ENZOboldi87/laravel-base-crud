652142e36c0edb29c4f5e7fc610cc12ce523e6fa
Giuseppe Rotolo <rotolo37@gmail.com>
2020-09-04 16:31:15 +0200

    agg

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -3,33 +3,25 @@
   BoldiFlix - Qui trovi solo il trash del trash
 @endsection
 @section('content')
-<h1>ciao sono i miei film presenti nel database</h1>
 
-<h2>questi sono i tuoi film</h2>
-<ul class="list-unstyled">
-  @foreach ($movies as $movie)
-    <li>{{$movie->title}}
-      <a href="{{route ('movies.show', $movie) }}"><button type="button" class="btn btn-primary">Dettagli</button></a>
-      <a href="{{route ('movies.edit', $movie) }}"><button type="button" class="btn btn-warning">Modifica</button></a>
-        <form action="{{ route('movies.destroy', $movie) }}" method="post">
-  					@csrf
-  					@method('DELETE')
+<h1>Questi sono i film presenti nel database</h1>
 
-  					<button type="submit" class="btn btn-danger">Elimina</button>
-  				</form>
+  <ul class="list-unstyled">
+    @foreach ($movies as $movie)
+      <li>{{$movie->title}}
+        <a href="{{route ('movies.show', $movie) }}"><button type="button" class="btn btn-primary">Dettagli</button></a>
+        <a href="{{route ('movies.edit', $movie) }}"><button type="button" class="btn btn-warning">Modifica</button></a>
+          <form action="{{ route('movies.destroy', $movie) }}" method="post">
+    					@csrf
+    					@method('DELETE')
 
-      {{-- <button type="submit" class="btn btn-info">
-        <span class="glyphicon glyphicon-trash"></span> Delete --}}
+    					<button type="submit" class="btn btn-danger">Elimina</button>
+    			</form>
+       </li>
+    @endforeach
+  </ul>
 
-
-     </li>
-  @endforeach
-
-</ul>
-
-<div class="">
-  <a href="{{route ('movies.create')}}">Aggiungi nuovo film</a>
-
-</div>
-
+  <div class="">
+    <a href="{{route ('movies.create')}}">Aggiungi nuovo film</a>
+  </div>
 @endsection
